b7bd7a53ee53199e9a8e3cf3120102f9d2800ffc
moeinhe83 <moeinheshmati1383@gmail.com>
2025-09-24 23:50:58 +0330

    Upload Get map with python project on Github

--- map.py
+++ map.py
@@ -1,14 +1,14 @@
 #  World Get Map App
 
-# Library 
-import folium 
+# Library
+import folium
 
-# Get Map 
+# Get Map
 map = folium.Map(tiles='cartodb positron')
 
 # Save File HTML
 map.save('map.html')
 
-
+# moeinit.com
 # Create By Moein Heshmati
-# Finish 
+# Finish
